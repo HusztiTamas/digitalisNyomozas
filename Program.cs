d2c8843d221c590dbc6bf8ffc8c1f107a790624d
Huszti Tamás <huszti.tamas@petrik.hu>
2026-01-29 13:52:04 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace digitalisNyomozas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

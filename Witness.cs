d5d95122d662093373aaf667467c626f61a53ba8
Huszti Tamás <huszti.tamas@petrik.hu>
2026-01-29 14:32:46 +0100

    Witness osztály elkezdése

---
+++ Witness.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace digitalisNyomozas
+{
+	internal class Witness
+	{
+		private string vallomasSzovege;
+		private DateTime vallomasdatuma;
+
+		public Witness(string vallomasSzovege, DateTime vallomasdatuma)
+		{
+			this.vallomasSzovege = vallomasSzovege;
+			this.vallomasdatuma = vallomasdatuma;
+		}
+
+		public string VallomasSzovege { get => vallomasSzovege; set => vallomasSzovege = value; }
+		public DateTime Vallomasdatuma { get => vallomasdatuma; set => vallomasdatuma = value; }
+	}
+}

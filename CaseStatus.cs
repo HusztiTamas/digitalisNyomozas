b5b8e0a782554068c68d8a9d45b2fa97634cf444
Huszti Tamás <huszti.tamas@petrik.hu>
2026-01-29 14:26:25 +0100

    CaseStatus osztály elkezdése

---
+++ CaseStatus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace digitalisNyomozas
+{
+	internal class CaseStatus
+	{
+		private string aktStatus;
+
+		public CaseStatus(string aktStatus)
+		{
+			this.aktStatus = aktStatus;
+		}
+
+		public string AktStatus { get => aktStatus; set => aktStatus = value; }
+	}
+}

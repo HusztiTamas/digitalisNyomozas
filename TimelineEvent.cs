3a560d3583fd2f31ec0a3e1156c3c5e688f389eb
Huszti Tamás <huszti.tamas@petrik.hu>
2026-01-29 14:39:04 +0100

    TimelineEvent osztály

---
+++ TimelineEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace digitalisNyomozas
+{
+	internal class TimelineEvent
+	{
+		private DateTime datum;
+		private string leiras;
+
+		public TimelineEvent(DateTime datum, string leiras)
+		{
+			this.datum = datum;
+			this.leiras = leiras;
+		}
+
+		public DateTime Datum { get => datum; set => datum = value; }
+		public string Leiras { get => leiras; set => leiras = value; }
+	}
+}
